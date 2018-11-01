29255d2e4a49a9594bab803aa846ba891d96fa93
Jacob Hinrichsen <jacob.hinrichsen@gmail.com>
2018-11-01 11:21:13 +0100

    Order cache created and implemented

--- src/main/java/cache/OrderCache.java
+++ src/main/java/cache/OrderCache.java
@@ -1,6 +1,35 @@
 package cache;
 
-//TODO: Build this cache and use it.
+import controllers.OrderController;
+import model.Order;
+import model.Product;
+import utils.Config;
+
+import java.util.ArrayList;
+
+//TODO: Build this cache and use it FIX
 public class OrderCache {
+
+    private ArrayList<Order> orders;
+
+    private long ttl;
+
+    private long created;
+
+    public OrderCache (){
+        this.ttl = Config.getOrderTtl();
+    }
+
+    public ArrayList<Order> getOrders (Boolean forceUpdate) {
+
+        if (forceUpdate || ((this.created + this.ttl) >= (System.currentTimeMillis()/1000L)) || this.orders == null) {
+
+            this.orders = OrderController.getOrders();
+
+            this.created = System.currentTimeMillis() / 1000L;
+
+        }
+        return this.orders;
+    }
 
 }
