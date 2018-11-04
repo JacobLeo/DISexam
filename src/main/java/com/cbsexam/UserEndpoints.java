f20472577e43c0c9dd99da8dd3053ee6880fccd1
Jacob Hinrichsen <jacob.hinrichsen@gmail.com>
2018-11-04 15:12:47 +0100

    login system

--- src/main/java/com/cbsexam/UserEndpoints.java
+++ src/main/java/com/cbsexam/UserEndpoints.java
@@ -84,7 +84,7 @@ public class UserEndpoints {
     }
   }
 
-  // TODO: Make the system able to login users and assign them a token to use throughout the system. fix 
+  // TODO: Make the system able to login users and assign them a token to use throughout the system. fix
   @POST
   @Path("/login")
   @Consumes(MediaType.APPLICATION_JSON)
